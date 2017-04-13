866697cc1e0050df35e5f387d40d971c75cc2c24
Carsen Yates <cyates@csumb.edu>
2017-04-13 21:28:10 +0000

    cart

--- functions.php
+++ functions.php
@@ -304,15 +304,10 @@ function makeWorldSearch($filter_name, $sort_by, $order)
     
     $result = '';
     $result.='<div id="search_results"><table align="center"><tr><tbody>';
-    $result.="<th></th>";
-    $result.="<th></th>";
-    $result.="<th></th>";
     $result.="<th>Name</th>";
-    $result.="<th>Year</th>";
-    $result.="<th>Characters</th>";
-    $result.="<th>Platform(s)</th>";
-    $result.="<th>Average play time</th>";
-    $result.="<th>Price</th><tr>";
+    $result.="<th>Towns</th>";
+    $result.="<th>Capital</th>";
+    $result.="<th>Game</th>";
     while($k->fetch())
     {
         
@@ -354,15 +349,82 @@ function makeWorldResult($id, $name, $num_towns, $game_id, $gamename, $capital)
 
 function makeCart($cart)
 {
-    // $cart should be an array of integers
-    //TODO
-    $result ='NOT YET IMPLEMENTED';
-    $result.='';
+    $db = new mysqli("localhost","team_project","","team_project");
+    if(count($cart)==0)
+    {
+        return "Your cart is empty.";
+    }
+    if ($db->connect_errno)
+    {
+        return "Sorry, this website is experiencing problems.<br/>".
+        "Error: Failed to make a MySQL connection, here is why: <br/>".
+        "Errno: " . $db->connect_errno . "<br/>".
+        "Error: " . $db->connect_error . "<br/>";
+    }
+    $result ='';
+    $result.='<table><tbody>';
+    $result.='<tr>';
+    
+    $result.='<th>';
+    
+    $result.='</th>';
+    
+    $result.='</tr>';
+    $query = 'SELECT * FROM game;';
+    foreach($cart as $item)
+    {
+        $k = $db->prepare($query);
+        $k->bind_param("i", $item);
+        $result.=makeCartResult($id, $name, $year, $num_chars, $platforms, $avg_play_time, $price);
+    }
+    $result.='<tbody></table>';
     
     return $result;
     
 }
 
+function makeCartResult($id, $name, $year, $num_chars, $platforms, $avg_play_time, $price)
+{
+    $result = '';
+    $result.='<tr>';
+        $result.='<td>';
+            $result.='<form>';
+            $result.='<input type="hidden" name="result_clicked" value="'.$id.'">';
+            $result.='<button class="button">View</button>';
+            $result.='</form>';
+        $result.='</td>';
+        $result.='<td>';
+            $result.='<form>';
+            $result.='<input type="hidden" name="result_added" value="'.$id.'">';
+            $result.='<button class="button">Add to cart</button>';
+            $result.='</form>';
+        $result.='</td>';
+        $result.='<td>';
+            $result.='<img src="img/games/';
+            $result.=$id;
+            $result.='.png"/>';
+        $result.='</td>';
+        $result.='<td>';
+            $result.=$name;
+        $result.='</td>';
+        $result.='<td>';
+            $result.=$year;
+        $result.='</td>';
+        $result.='<td>';
+            $result.=$num_chars;
+        $result.='</td>';
+        $result.='<td>';
+            $result.=$platforms;
+        $result.='</td>';
+        $result.='<td>';
+            $result.=$avg_play_time;
+        $result.=' hours</td>';
+        $result.='<td>$';
+            $result.=$price;
+        $result.='</td>';
+    $result.='</tr>';
+    return $result;
+}
 function makeItemInfo($item)
 {
     $db = new mysqli("localhost","team_project","","team_project");
